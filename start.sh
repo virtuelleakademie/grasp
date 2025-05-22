af41d323e6cca2fcfa549ed7855c5f7a078d1bf3
Andrew Ellis <andrewellis@pm.me>
2025-05-22 17:11:44 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-chainlit run app.py --host 0.0.0.0 --port 10000
+
+if [ -n "$1" ]; then
+  # If argument is provided, use it as EXERCISE_NAME
+  EXERCISE_NAME="$1" chainlit run app.py --host 0.0.0.0 --port 10000
+else
+  # Otherwise run without the environment variable
+  chainlit run app.py --host 0.0.0.0 --port 10000
+fi
